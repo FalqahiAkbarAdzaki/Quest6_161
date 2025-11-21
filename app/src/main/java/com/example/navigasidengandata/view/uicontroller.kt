7b4f1bf71802887412d98182361ee4abd12bdfb5
FalqahiAkbarAdzaki <dragontamer1925@gmail.com>
2025-11-21 16:02:00 +0700

    14

--- app/src/main/java/com/example/navigasidengandata/view/uicontroller.kt
+++ app/src/main/java/com/example/navigasidengandata/view/uicontroller.kt
@@ -5,6 +5,9 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
