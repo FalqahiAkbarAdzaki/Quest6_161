2f432f38c33e680f447339ad1f031da9976d6b73
FalqahiAkbarAdzaki <dragontamer1925@gmail.com>
2025-11-21 10:28:49 +0700

    2

--- app/src/main/java/com/example/navigasidengandata/viewmodel/SiswaViewmodel.kt
+++ app/src/main/java/com/example/navigasidengandata/viewmodel/SiswaViewmodel.kt
@@ -5,12 +5,13 @@ import com.example.navigasidengandata.model.Siswa
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
 
 class SiswaViewmodel : ViewModel() {
     private val _statusUI = MutableStateFlow(Siswa())
     val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
 
     fun setSiswa(ls:MutableList<String>){
-        _statusUI
+        _statusUI.update { statusSaatIni } ->
     }
 }
